5de6962ebefff55e60952e98dd941128f4cde9c7
Ryan Tablada <ryan.tablada@gmail.com>
2018-02-19 17:25:22 -0600

    Starting App

---
+++ test/library_api_web/views/error_view_test.exs
@@ -0,0 +1,21 @@
+defmodule LibraryApiWeb.ErrorViewTest do
+  use LibraryApiWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.json" do
+    assert render(LibraryApiWeb.ErrorView, "404.json", []) ==
+           %{errors: %{detail: "Page not found"}}
+  end
+
+  test "render 500.json" do
+    assert render(LibraryApiWeb.ErrorView, "500.json", []) ==
+           %{errors: %{detail: "Internal server error"}}
+  end
+
+  test "render any other" do
+    assert render(LibraryApiWeb.ErrorView, "505.json", []) ==
+           %{errors: %{detail: "Internal server error"}}
+  end
+end
